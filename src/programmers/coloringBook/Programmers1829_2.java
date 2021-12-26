1248f5b8e5e0978af600a13a4ead75a0d886778e
Astronaut <hsymyth@gmail.com>
2021-12-26 23:27:41 +0900

    programmers 1829

--- src/programmers/coloringBook/Programmers1829_2.java
+++ src/programmers/coloringBook/Programmers1829_2.java
@@ -26,17 +26,17 @@ public class Programmers1829_2 {
     public int[] solution(int m, int n, int[][] picture) {
         numberOfArea = 0;
         maxSizeOfOneArea = 0;
-        int[] answer = new int[2];
-        answer[0] = numberOfArea;
-        answer[1] = maxSizeOfOneArea;
+        int[] answer = {numberOfArea, maxSizeOfOneArea};
         boolean[][] check = new boolean[m][n];
-        for(int i =0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(picture[i][j] != 0 && !check[i][j]){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (picture[i][j] != 0 && !check[i][j]) {
                     numberOfArea++;
-                    dfs(i,j,picture,check);
+                    dfs(i, j, picture, check);
                 }
-                if(temp_cnt > maxSizeOfOneArea) maxSizeOfOneArea = temp_cnt;
+                if (temp_cnt > maxSizeOfOneArea) {
+                    maxSizeOfOneArea = temp_cnt;
+                }
                 temp_cnt = 0;
             }
         }
