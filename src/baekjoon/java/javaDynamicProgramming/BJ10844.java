d07ce469cc642e269d53f210b04a70a838ea14a5
Astronaut <hsymyth@gmail.com>
2022-02-10 01:12:54 +0900

    feat : more dynamic programming problems

---
+++ src/baekjoon/java/javaDynamicProgramming/BJ10844.java
@@ -0,0 +1,12 @@
+package baekjoon.java.javaDynamicProgramming;
+
+public class BJ10844 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static Long stairNum(int num) {
+        return null;
+    }
+}
---
+++ src/baekjoon/java/javaDynamicProgramming/BJ9461.java
@@ -0,0 +1,38 @@
+package baekjoon.java.javaDynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ9461 {
+
+    static Long[] longArray = new Long[101];
+
+    public static void main(String[] args) throws IOException {
+        longArray[1] = 1L;
+        longArray[2] = 1L;
+        longArray[3] = 1L;
+        longArray[4] = 2L;
+        longArray[5] = 2L;
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(padovan(Integer.parseInt(bf.readLine()))).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static Long padovan(int num) {
+        if (num <= 5) {
+            return longArray[num];
+        }
+
+        if (longArray[num] == null) {
+            longArray[num] = padovan(num - 1) + padovan(num - 5);
+        }
+        return longArray[num];
+    }
+
+}
