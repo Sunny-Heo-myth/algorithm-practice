97cf2f3be11efe6544c99d98bff25d0839e61b0c
Alan <alan@g1project.net>
2023-07-05 23:43:49 +0900

    exercise: o.a.algo.bj.BJ10988

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,5 +1,6 @@
 package org.alan.algorithm.practice.baekjoon;
 
+import org.alan.algorithm.practice.baekjoon.stepbystep.deepone.BJ10988;
 import org.alan.algorithm.practice.baekjoon.stepbystep.deepone.BJ2444;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
@@ -8,9 +9,9 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int i = Integer.parseInt(IOUtil.readFiniteLine(s->0));
-        BJ2444 instance = new BJ2444();
-        String answer = instance.solve(i);
+        String input = IOUtil.readFiniteLine(s->0);
+        BJ10988 instance = new BJ10988();
+        int answer = instance.solve(input);
         System.out.print(answer);
     }
 
