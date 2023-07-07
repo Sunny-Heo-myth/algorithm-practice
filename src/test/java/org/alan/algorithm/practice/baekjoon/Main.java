2ba5b94d10fb30fc0d54a72e95afdb9b0c039563
Alan <alan@g1project.net>
2023-07-07 19:05:12 +0900

    exercise BJ11005

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,6 +1,7 @@
 package org.alan.algorithm.practice.baekjoon;
 
 import org.alan.algorithm.practice.baekjoon.stepbystep.deepone.BJ25206;
+import org.alan.algorithm.practice.baekjoon.stepbystep.math1step.BJ11005;
 import org.alan.algorithm.practice.baekjoon.stepbystep.math1step.BJ2745;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
@@ -10,8 +11,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         String input = IOUtil.readFiniteLine(s->0);
-        BJ2745 instance = new BJ2745();
-        int answer = instance.solve(input);
+        BJ11005 instance = new BJ11005();
+        var answer = instance.solve(input);
         System.out.print(answer);
     }
 
--- src/test/java/org/alan/algorithm/practice/baekjoon/stepbystep/math1step/BJ2745Test.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/stepbystep/math1step/BJ2745Test.java
@@ -1,6 +1,5 @@
 package org.alan.algorithm.practice.baekjoon.stepbystep.math1step;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
