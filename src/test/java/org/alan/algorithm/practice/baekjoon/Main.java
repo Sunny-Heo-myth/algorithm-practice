1b8fd890f84e24f101ca6f1ce2e3112a6001cd21
Alan <alan@g1project.net>
2023-07-07 15:15:37 +0900

    exercise: BJ2745

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,6 +1,7 @@
 package org.alan.algorithm.practice.baekjoon;
 
 import org.alan.algorithm.practice.baekjoon.stepbystep.deepone.BJ25206;
+import org.alan.algorithm.practice.baekjoon.stepbystep.math1step.BJ2745;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
@@ -8,9 +9,9 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String input = IOUtil.readFiniteLine(s->19);
-        BJ25206 instance = new BJ25206();
-        double answer = instance.solve(input);
+        String input = IOUtil.readFiniteLine(s->0);
+        BJ2745 instance = new BJ2745();
+        int answer = instance.solve(input);
         System.out.print(answer);
     }
 
