a9086a75c65cf6226b10dccde53a0d7dbfb54ded
Alan <alan@g1project.net>
2023-07-10 17:05:57 +0900

    exercise: o.a.algo.bj.2501

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/stepbystep/factormultipleprime/BJ2501.java
@@ -0,0 +1,36 @@
+package org.alan.algorithm.practice.baekjoon.stepbystep.factormultipleprime;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BJ2501 {
+    public String solve(String input) {
+        String[] s = input.split(" ");
+        int dividend = Integer.parseInt(s[0]);
+        int order = Integer.parseInt(s[1]);
+
+        List<Integer> factors = new LinkedList<>();
+        double sqrt = Math.sqrt(dividend);
+
+        int divisor = 1;
+        while (divisor <= sqrt) {
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            if (remainder == 0) {
+                if (quotient != sqrt) {
+                    factors.add(quotient);
+                }
+                factors.add(divisor);
+            }
+            divisor++;
+        }
+
+        if (order > factors.size()) {
+            return String.valueOf(0);
+        } else {
+            return String.valueOf(factors.stream().sorted().collect(Collectors.toList()).get(order - 1));
+        }
+    }
+}
--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,6 +1,6 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.stepbystep.factormultipleprime.BJ5086;
+import org.alan.algorithm.practice.baekjoon.stepbystep.factormultipleprime.BJ2501;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
@@ -8,11 +8,8 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BJ5086 instance = new BJ5086();
-        String answer = IOUtil.answerWithEndLineCondition(s -> {
-            String[] strings = s.split(" ");
-            return Integer.parseInt(strings[0]) == 0 && Integer.parseInt(strings[1]) == 0;
-        }, instance::solve);
+        BJ2501 instance = new BJ2501();
+        String answer = IOUtil.answer(instance::solve);
         System.out.print(answer);
     }
 
