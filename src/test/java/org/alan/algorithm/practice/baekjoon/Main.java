992b0284af96b8dc817f1d306501bfdd7f4660c4
Alan <alan@g1project.net>
2023-08-31 10:31:20 +0900

    exercise: BJ2485

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,6 +1,6 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.stepbystep.factormultipleprime2.BJ1735;
+import org.alan.algorithm.practice.baekjoon.stepbystep.factormultipleprime2.BJ2485;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
@@ -8,8 +8,8 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BJ1735 instance = new BJ1735();
-        String input = IOUtil.readFiniteLine(2);
+        BJ2485 instance = new BJ2485();
+        String input = IOUtil.readFiniteLine(Integer::parseInt);
         System.out.print(instance.solve(input));
     }
 
