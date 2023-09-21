b6972da0c38913173dd6eeb20683f5520566aae6
Alan <alan@g1project.net>
2023-09-21 10:57:02 +0900

    exercise: BJ24511

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,6 +1,6 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.stepbystep.stackqueuedequestep.BJ2346;
+import org.alan.algorithm.practice.baekjoon.stepbystep.stackqueuedequestep.BJ24511;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
@@ -8,8 +8,8 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BJ2346 instance = new BJ2346();
-        IOUtil.answer(1, instance::solve);
+        BJ24511 instance = new BJ24511();
+        IOUtil.answer(4, instance::solve);
     }
 
 }
