23e70acda0dfdc1e497b8d205102e20c7805f5cf
sunny <hsymyth@gmail.com>
2023-09-24 18:27:49 +0900

    exercise: BJ1037

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/stepbystep/deeptwo/BJ1037.java
@@ -0,0 +1,85 @@
+package org.alan.algorithm.practice.baekjoon.stepbystep.deeptwo;
+
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BJ1037 {
+    public String solve(String input) {
+        // parse
+        List<Long> ints = Arrays.stream(input.split("\n")[1].split(" "))
+                .mapToLong(Long::parseLong)
+                .sorted()   // O(nlgn)
+                .boxed()
+                .collect(Collectors.toCollection(LinkedList::new));
+
+        // filter multiples O(n**2)
+//        int i = 0;
+//        while (i < ints.size()) {
+//            int j = i + 1;
+//            boolean deleted = false;
+//            while (j < ints.size()) {
+//                if (ints.get(j) % ints.get(i) == 0) {
+//                    ints.remove(i);
+//                    deleted = true;
+//                    break;
+//                }
+//                j++;
+//            }
+//            if (!deleted) {
+//                i++;
+//            }
+//        }
+
+        // get lcm with gcd by Euclidean O(n) ?
+        long lcm = getLcm(ints);
+
+        if (lcm == ints.get(ints.size() - 1)) {
+            return String.valueOf(lcm * 2);
+        }
+        return String.valueOf(lcm);
+    }
+
+    private long getLcm(List<Long> ints) {
+        long lcm = ints.get(0);
+        for (int i = 1; i < ints.size(); i++) {
+            lcm = getLcm(lcm, ints.get(i));
+        }
+        return lcm;
+    }
+
+    private long getLcm(long a, long b) {
+        return a / getGcd(a, b) * b;
+    }
+
+    private long getGcd(long a, long b) {
+        long small, big;
+        if (a >= b) {
+            big = a;
+            small = b;
+        }
+        else {
+            small = a;
+            big = b;
+        }
+
+        while (small != 0) {
+            long remainder = big % small;
+            big = small;
+            small = remainder;
+        }
+        return big;
+    }
+
+    // todo: read the question carefully
+    public String solve2(String input) {
+        int[] ints = Arrays.stream(input.split("\n")[1].split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()   // O(nlgn)
+                .toArray();
+
+        return String.valueOf(ints[0] * ints[ints.length - 1]);
+    }
+}
--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,6 +1,6 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.problem.BJ1850;
+import org.alan.algorithm.practice.baekjoon.stepbystep.deeptwo.BJ1037;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
@@ -8,8 +8,8 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BJ1850 instance = new BJ1850();
-        IOUtil.answer(instance::solve);
+        BJ1037 instance = new BJ1037();
+        IOUtil.answer(1, instance::solve2);
     }
 
 }
