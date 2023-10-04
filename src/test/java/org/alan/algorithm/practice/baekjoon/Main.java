7d5714bbc9394b7a01d7e7831529ac77e5c17fd5
Alan <alan@g1project.net>
2023-10-04 13:48:37 +0900

    exercise: BJ1259

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,15 +1,16 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.stepbystep.recursionstep.BJ11729;
+import org.alan.algorithm.practice.baekjoon.problem.BJ1259;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BJ11729 instance = new BJ11729();
-        IOUtil.answer(instance::solve);
+        BJ1259 instance = new BJ1259();
+        IOUtil.answerQuestionsWithEndLineCondition(s -> Objects.equals(s, "0"), instance::solve);
     }
 
 }
---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/problem/BJ1259Test.java
@@ -0,0 +1,25 @@
+package org.alan.algorithm.practice.baekjoon.problem;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ1259Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("121", "yes"),
+                Arguments.of("1231", "no"),
+                Arguments.of("12421", "yes")
+        );
+    }
+    
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        BJ1259 instance = new BJ1259();
+        Assertions.assertEquals(expected, instance.solve(input));
+    }
+}
