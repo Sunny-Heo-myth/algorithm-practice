846e926164f1bc28298617d61e238a2a4ae4510e
Alan <alan@g1project.net>
2023-10-04 18:07:59 +0900

    exercise: BJ1920

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,16 +1,15 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.problem.BJ1259;
+import org.alan.algorithm.practice.baekjoon.problem.BJ1920;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BJ1259 instance = new BJ1259();
-        IOUtil.answerQuestionsWithEndLineCondition(s -> Objects.equals(s, "0"), instance::solve);
+        BJ1920 instance = new BJ1920();
+        IOUtil.answer(3, instance::solve);
     }
 
 }
---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/problem/BJ1920Test.java
@@ -0,0 +1,30 @@
+package org.alan.algorithm.practice.baekjoon.problem;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ1920Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("5\n" +
+                        "4 1 5 2 3\n" +
+                        "5\n" +
+                        "1 3 7 9 5", "1\n" +
+                        "1\n" +
+                        "0\n" +
+                        "0\n" +
+                        "1")
+        );
+    }
+    
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        BJ1920 instance = new BJ1920();
+        Assertions.assertEquals(expected, instance.solve(input));
+    }
+}
