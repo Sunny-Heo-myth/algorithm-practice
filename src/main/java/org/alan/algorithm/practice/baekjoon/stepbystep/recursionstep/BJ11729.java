84be6ee8eff9c0ada869524555ff478728a31737
Alan <alan@g1project.net>
2023-10-04 10:25:07 +0900

    exercise: BJ11729

--- src/main/java/org/alan/algorithm/practice/baekjoon/stepbystep/recursionstep/BJ11729.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/stepbystep/recursionstep/BJ11729.java
@@ -1,28 +1,28 @@
 package org.alan.algorithm.practice.baekjoon.stepbystep.recursionstep;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class BJ11729 {
 
-    static StringBuilder sb = new StringBuilder();
+    private StringBuilder sb;
 
-    public static void main(String[] args) throws IOException {
-        int x = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
-        sb.append((int) (Math.pow(2, x) - 1)).append("\n");
-        hanoi(1, 2, 3, x);
-        System.out.println(sb);
+    public String solve(String input) {
+        int n = Integer.parseInt(input);
+        sb = new StringBuilder();
+
+        sb.append((int) Math.pow(2, n) - 1).append("\n");
+
+        hanoi(1, 2, 3, n);
+
+        return sb.deleteCharAt(sb.length() - 1).toString();
     }
 
-
-    static void hanoi(int from, int mid, int to, int n) {
+    private void hanoi(int from, int mid, int to, int n) {
         if (n == 1) {
-            sb.append(from + " " + to + "\n");
+            sb.append(from).append(" ").append(to).append("\n");
             return;
         }
+
         hanoi(from, to, mid, n - 1);
-        sb.append(from + " " + to + "\n");
+        sb.append(from).append(" ").append(to).append("\n");
         hanoi(mid, from, to, n - 1);
     }
 }
--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,6 +1,6 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.stepbystep.dynamicprogrammingstep.BJ24416;
+import org.alan.algorithm.practice.baekjoon.stepbystep.recursionstep.BJ11729;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BJ24416 instance = new BJ24416();
+        BJ11729 instance = new BJ11729();
         IOUtil.answer(instance::solve);
     }
 
