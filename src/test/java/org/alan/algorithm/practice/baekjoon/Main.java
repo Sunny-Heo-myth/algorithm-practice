abf87788cd55316371ac7b5b0d662095ea8be225
Alan <alan@g1project.net>
2023-10-06 15:37:10 +0900

    exercise: BJ11399

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,6 +1,6 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.level.two.BJ10866;
+import org.alan.algorithm.practice.baekjoon.level.three.BJ11399;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
@@ -8,8 +8,8 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BJ10866 instance = new BJ10866();
-        IOUtil.answer(Integer::parseInt, instance::solve);
+        BJ11399 instance = new BJ11399();
+        IOUtil.answer(1, instance::solve);
     }
 
 }
