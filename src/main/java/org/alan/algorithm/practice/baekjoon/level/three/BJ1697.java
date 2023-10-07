fba779792c94523373272e92fd30c6d70b8f5686
sunny <hsymyth@gmail.com>
2023-10-07 17:02:46 +0900

    exercise: BJ1697

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/level/three/BJ1697.java
@@ -0,0 +1,45 @@
+package org.alan.algorithm.practice.baekjoon.level.three;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BJ1697 {
+    private final Queue<Integer> queue = new LinkedList<>();
+//    private int endIndex;
+    private int[] counts = new int[100001];
+
+    public String solve(String input) {
+        String[] s = input.split(" ");
+        int x = Integer.parseInt(s[0]);
+        int y = Integer.parseInt(s[1]);
+        // I always prefer endIndex is not covered.
+//        endIndex = Math.min((y - 1) * 2 + 1, 100001);
+//        counts = new int[endIndex];
+
+        int answer = move(x, y);
+        return String.valueOf(answer);
+    }
+
+    private int move(int from, int to) {
+        queue.add(from);
+        while (!queue.isEmpty()) {
+            int next = queue.poll();
+
+            if (next == to) return counts[next];
+
+            int nextCount = counts[next] + 1;
+            conditionedOffer(next * 2, nextCount);
+            conditionedOffer(next + 1, nextCount);
+            conditionedOffer(next - 1, nextCount);
+        }
+        return -1;
+    }
+
+    private void conditionedOffer(int index, int step) {
+        if (index >= 0 && index < 100001 && counts[index] == 0){
+            counts[index] = step;
+            queue.offer(index);
+        }
+    }
+
+}
--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,6 +1,6 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.level.three.BJ1074;
+import org.alan.algorithm.practice.baekjoon.level.three.BJ1697;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BJ1074 instance = new BJ1074();
+        BJ1697 instance = new BJ1697();
         IOUtil.answer(instance::solve);
     }
 
