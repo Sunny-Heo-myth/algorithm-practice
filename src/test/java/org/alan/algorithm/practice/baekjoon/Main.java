efc96556136427c9704aa3395928e9560be480f3
sunny <hsymyth@gmail.com>
2023-10-07 11:13:58 +0900

    exercise: BJ1074

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,6 +1,6 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.level.three.BJ11399;
+import org.alan.algorithm.practice.baekjoon.level.three.BJ1074;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
@@ -8,8 +8,8 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BJ11399 instance = new BJ11399();
-        IOUtil.answer(1, instance::solve);
+        BJ1074 instance = new BJ1074();
+        IOUtil.answer(instance::solve);
     }
 
 }
