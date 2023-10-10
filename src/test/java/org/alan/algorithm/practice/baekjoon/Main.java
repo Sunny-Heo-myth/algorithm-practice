d3d1e6efd81d7fa53ee8835c68e452d8ae3a7421
Alan <alan@g1project.net>
2023-10-10 10:42:27 +0900

    exercise: BJ1931

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,14 +1,14 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.stepbystep.cumulativesumstep.BJ11659;
+import org.alan.algorithm.practice.baekjoon.level.three.BJ1931;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BJ11659 instance = new BJ11659();
-        IOUtil.answer(input -> Integer.parseInt(input.split(" ")[1]) + 1, instance::solve2);
+        BJ1931 instance = new BJ1931();
+        IOUtil.answer(Integer::parseInt, instance::solve);
     }
 
 }
