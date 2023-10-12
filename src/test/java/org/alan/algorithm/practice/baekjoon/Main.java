bea133afceb7729d45e87a7d320e1f2db155c914
Alan <alan@g1project.net>
2023-10-12 10:07:23 +0900

    exercise: BJ2630

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,9 +1,15 @@
 package org.alan.algorithm.practice.baekjoon;
 
+import org.alan.algorithm.practice.baekjoon.level.three.BJ2630;
+import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BJ2630 instance = new BJ2630();
+        IOUtil.answer(Integer::parseInt, instance::solve);
     }
+
 
 }
