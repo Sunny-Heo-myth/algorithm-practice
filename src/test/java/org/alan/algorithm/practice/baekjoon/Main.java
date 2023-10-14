8c172d36b9dac23de90a59c1bb5eb0554b8b3067
sunny <hsymyth@gmail.com>
2023-10-15 07:53:07 +0900

    exercise: BJ11047

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,14 +1,14 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.level.four.BJ1167;
+import org.alan.algorithm.practice.baekjoon.tagged.greedy.BJ11047;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BJ1167 instance = new BJ1167();
-        IOUtil.answer(Integer::parseInt, instance::solve);
+        BJ11047 instance = new BJ11047();
+        IOUtil.answer(s -> Integer.parseInt(s.split(" ")[0]), instance::solve);
     }
 
 
