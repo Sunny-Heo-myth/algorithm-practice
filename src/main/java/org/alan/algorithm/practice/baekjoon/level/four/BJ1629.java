56d153aec29de4afe7360875237429496b55d525
Alan <alan@g1project.net>
2023-10-17 23:19:04 +0900

    exercise: BJ1629

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/level/four/BJ1629.java
@@ -0,0 +1,28 @@
+package org.alan.algorithm.practice.baekjoon.level.four;
+
+import java.util.Arrays;
+
+/*
+(a * b) % c = (a % c * b % c) % c
+ */
+public class BJ1629 {
+    private long base = 0;
+    private long divisor = 0;
+    public String solve(String input) {
+        long[] longs = Arrays.stream(input.split(" ")).mapToLong(Long::parseLong).toArray();
+        base = longs[0];
+        if (base == divisor) return "0";
+        if (base == 1 && divisor > 1) return "1";
+
+        long power = longs[1];
+        divisor = longs[2];
+        return String.valueOf(recur(power));
+    }
+
+    private long recur(long power) {
+        if (power == 1) return base;
+        long half = recur(power / 2);
+        if (power % 2 == 0) return (half % divisor * half % divisor) % divisor;
+        else return ((half * half) % divisor * (base % divisor)) % divisor;
+    }
+}
--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,14 +1,14 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.level.four.BJ1238;
+import org.alan.algorithm.practice.baekjoon.level.four.BJ1629;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BJ1238 instance = new BJ1238();
-        IOUtil.answer(s -> Integer.parseInt(s.split(" ")[1]), instance::solve);
+        BJ1629 instance = new BJ1629();
+        IOUtil.answer(instance::solve);
     }
 
 
