0dad78b82815fe275523d11b6ad913ed989a91d5
sunny <hsymyth@gmail.com>
2023-10-19 08:13:33 +0900

    exercise: BJ2559

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/stepbystep/cumulativesumstep/BJ2559.java
@@ -0,0 +1,20 @@
+package org.alan.algorithm.practice.baekjoon.stepbystep.cumulativesumstep;
+
+import java.util.Arrays;
+
+public class BJ2559 {
+    public String solve(String input) {
+        String[] s = input.split("\n");
+        int count = Integer.parseInt(s[0].split(" ")[1]);
+        int[] array = Arrays.stream(s[1].split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int sum = 0;
+        int[] sums = new int[array.length];
+        for (int i = 0; i < sums.length; i++) sums[i] = sum += array[i];
+
+        int max = sums[count - 1];
+        for (int i = count; i < sums.length; i++) max = Integer.max(max, sums[i] - sums[i - count]);
+
+        return String.valueOf(max);
+    }
+}
--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,14 +1,14 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.level.four.BJ1629;
+import org.alan.algorithm.practice.baekjoon.stepbystep.cumulativesumstep.BJ2559;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BJ1629 instance = new BJ1629();
-        IOUtil.answer(instance::solve);
+        BJ2559 instance = new BJ2559();
+        IOUtil.answer(1, instance::solve);
     }
 
 
