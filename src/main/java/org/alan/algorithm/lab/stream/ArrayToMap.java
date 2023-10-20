bc8ab9971c4e7f1f6c9610b46865c20e32b0eb59
Alan <alan@g1project.net>
2023-10-20 14:54:37 +0900

    review: BJ10986

---
+++ src/main/java/org/alan/algorithm/lab/stream/ArrayToMap.java
@@ -0,0 +1,18 @@
+package org.alan.algorithm.lab.stream;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ArrayToMap {
+
+    public void run() {
+        Long[] numbers = {1L, 2L, 3L, 2L, 3L, 3L};
+
+        Map<Long, Integer> frequencyMap = Arrays.stream(numbers)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.reducing(0, e -> 1, Integer::sum)));
+
+        System.out.print(frequencyMap);  // Outputs: {1=1, 2=2, 3=3}
+    }
+}
--- src/main/java/org/alan/algorithm/practice/baekjoon/stepbystep/cumulativesumstep/BJ10986.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/stepbystep/cumulativesumstep/BJ10986.java
@@ -1,27 +1,28 @@
 package org.alan.algorithm.practice.baekjoon.stepbystep.cumulativesumstep;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class BJ10986 {
 
     public String solve(String input) {
         String[] s = input.split("\n");
         int divisor = Integer.parseInt(s[0].split(" ")[1]);
-        long[] array = Arrays.stream(s[1].split(" ")).mapToLong(Long::parseLong).toArray();
 
-        for (int i = 1; i < array.length; i++) array[i] += array[i - 1];    // O(n)
-        for (int i = 0; i < array.length; i++) array[i] %= divisor;
+        Long[] array = Arrays.stream(s[1].split(" ")).map(Long::valueOf).toArray(Long[]::new);
+        for (int i = 1; i < array.length; i++) array[i] += array[i - 1];    // cumulate sum
+        for (int i = 0; i < array.length; i++) array[i] %= divisor; // calculate remainder
 
-        Map<Long, Integer> map = new HashMap<>();
-        for (long remainder : array)
-            if (map.containsKey(remainder)) map.put(remainder, map.get(remainder) + 1);
-            else map.put(remainder, 1);
+        Map<Long, Integer> map = Arrays.stream(array)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.reducing(0, i -> 1, Integer::sum)));
 
-        long count = 0; // should be long
-        if (map.containsKey(0L)) count += map.get(0L);
-        for (long value : map.values()) if (value > 1) count += value * (value - 1) / 2;
+        long count = map.getOrDefault(0L, 0);
+        count += map.values().stream()
+                .mapToLong(i -> i > 1 ? (long) i * (i - 1) / 2 : 0)
+                .sum();
+
         return String.valueOf(count);
     }
 }
