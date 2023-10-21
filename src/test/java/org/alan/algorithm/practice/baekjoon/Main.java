fe76d6d36bc9b3ac59ea3084652d0673c290e59c
Alan <alan@g1project.net>
2023-10-21 20:34:25 +0900

    review: BJ1753

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,17 +1,14 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.stepbystep.cumulativesumstep.BJ11660;
+import org.alan.algorithm.practice.baekjoon.level.four.BJ1753;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BJ11660 instance = new BJ11660();
-        IOUtil.answer(s -> {
-            String[] ss = s.split(" ");
-            return Integer.parseInt(ss[0]) + Integer.parseInt(ss[1]);
-        }, instance::solve);
+        BJ1753 instance = new BJ1753();
+        IOUtil.answer(s -> Integer.parseInt(s.split(" ")[1]) + 1, instance::solve);
     }
 
 
