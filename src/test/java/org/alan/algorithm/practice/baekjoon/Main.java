6feab553d33b248779c2346043dd776acc427431
Alan <alan@g1project.net>
2023-10-30 10:21:54 +0900

    exercise: BJ1967

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,14 +1,14 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.level.four.BJ1918;
+import org.alan.algorithm.practice.baekjoon.level.four.BJ1967;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BJ1918 instance = new BJ1918();
-        IOUtil.answer(instance::solve);
+        BJ1967 instance = new BJ1967();
+        System.out.print(instance.solve(IOUtil.readWithNoEndLineCondition()));
     }
 
 
