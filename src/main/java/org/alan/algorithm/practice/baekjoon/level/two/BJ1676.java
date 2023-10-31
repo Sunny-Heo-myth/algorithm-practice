8b944b87a64c696dc0c0a681334d7b884f816862
sunny <hsymyth@gmail.com>
2023-10-31 22:10:37 +0900

    exercise: BJ1676

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/level/two/BJ1676.java
@@ -0,0 +1,20 @@
+package org.alan.algorithm.practice.baekjoon.level.two;
+
+public class BJ1676 {
+    private int two = 0;
+    private int five = 0;
+    public String solve(String input) {
+        for (int i = 1; i <= Integer.parseInt(input); i++) {
+            int n = i;
+            while (n % 2 == 0) {
+                n /= 2;
+                two++;
+            }
+            while (n % 5 == 0) {
+                n /= 5;
+                five++;
+            }
+        }
+        return String.valueOf(Integer.min(two, five));
+    }
+}
--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,14 +1,14 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.level.four.BJ1967;
+import org.alan.algorithm.practice.baekjoon.level.two.BJ1676;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BJ1967 instance = new BJ1967();
-        System.out.print(instance.solve(IOUtil.readWithNoEndLineCondition()));
+        BJ1676 instance = new BJ1676();
+        IOUtil.answer(instance::solve);
     }
 
 
