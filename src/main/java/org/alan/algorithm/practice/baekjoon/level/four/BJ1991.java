17cfd2262f16ddf80c8d2bddcd2526d711f302d9
Alan <alan@g1project.net>
2023-11-01 16:57:08 +0900

    exercise: BJ1991

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/level/four/BJ1991.java
@@ -0,0 +1,73 @@
+package org.alan.algorithm.practice.baekjoon.level.four;
+
+public class BJ1991 {
+
+    private final Node root = Node.of('A', null, null);
+    private final StringBuilder sb = new StringBuilder();
+
+    public String solve(String input) {
+        String[] s = input.split("\n");
+        for (int i = 1; i < Integer.parseInt(s[0]) + 1; i++) {
+            String[] ss = s[i].split(" ");
+            insertNode(root, ss[0].charAt(0), ss[1].charAt(0), ss[2].charAt(0));
+        }
+
+        printPreOrder(root);
+        sb.append("\n");
+        printInOrder(root);
+        sb.append("\n");
+        printPostOrder(root);
+
+        return sb.toString();
+    }
+
+    private void insertNode(Node currentNode, char newValue, char newLeft, char newRight) {
+        if (currentNode.value == newValue) {
+            currentNode.left = newLeft == '.' ? null : Node.of(newLeft, null, null);
+            currentNode.right = newRight == '.' ? null : Node.of(newRight, null, null);
+            return;
+        }
+
+        if (currentNode.left != null) insertNode(currentNode.left, newValue, newLeft, newRight);
+        if (currentNode.right != null) insertNode(currentNode.right, newValue, newLeft, newRight);
+    }
+
+    private void printPreOrder(Node node) {
+        if (node == null) return;
+
+        sb.append(node.value);
+        printPreOrder(node.left);
+        printPreOrder(node.right);
+    }
+
+    private void printInOrder(Node node) {
+        if (node == null) return;
+
+        printInOrder(node.left);
+        sb.append(node.value);
+        printInOrder(node.right);
+    }
+
+    private void printPostOrder(Node node) {
+        if (node == null) return;
+
+        printPostOrder(node.left);
+        printPostOrder(node.right);
+        sb.append(node.value);
+    }
+
+    private static class Node {
+        private char value;
+        private Node left;
+        private Node right;
+
+        private static Node of(char value, Node left, Node right) {
+            Node newNode = new Node();
+            newNode.value = value;
+            newNode.left = left;
+            newNode.right = right;
+            return newNode;
+        }
+    }
+
+}
--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,14 +1,14 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.level.two.BJ1676;
+import org.alan.algorithm.practice.baekjoon.level.four.BJ1991;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BJ1676 instance = new BJ1676();
-        IOUtil.answer(instance::solve);
+        BJ1991 instance = new BJ1991();
+        IOUtil.answer(Integer::parseInt, instance::solve);
     }
 
 
