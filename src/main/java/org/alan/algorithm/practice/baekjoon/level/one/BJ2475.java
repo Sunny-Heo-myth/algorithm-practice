fb4b76ec63f5c1acf89d828a0712ceee30fbaadc
sunny <hsymyth@gmail.com>
2023-11-02 15:04:58 +0900

    exercise: BJ2475

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/level/one/BJ2475.java
@@ -0,0 +1,9 @@
+package org.alan.algorithm.practice.baekjoon.level.one;
+
+import java.util.Arrays;
+
+public class BJ2475 {
+    public String solve(String input) {
+        return String.valueOf(Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).map(i -> i * i).sum() % 10);
+    }
+}
--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,14 +1,14 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.level.four.BJ1991;
+import org.alan.algorithm.practice.baekjoon.level.one.BJ2475;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BJ1991 instance = new BJ1991();
-        IOUtil.answer(Integer::parseInt, instance::solve);
+        BJ2475 instance = new BJ2475();
+        IOUtil.answer(instance::solve);
     }
 
 
