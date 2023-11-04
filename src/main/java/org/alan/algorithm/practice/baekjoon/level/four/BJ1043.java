e133b89cf6f9c1f63ab4e694e800995de8b325e3
sunny <hsymyth@gmail.com>
2023-11-04 15:27:56 +0900

    exercise: BJ1043

--- src/main/java/org/alan/algorithm/practice/baekjoon/level/four/BJ1043.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/level/four/BJ1043.java
@@ -1,42 +1,66 @@
 package org.alan.algorithm.practice.baekjoon.level.four;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BJ1043 {
-    private int[] know;
-    private boolean[][] hp;
+    private int[] graph;
+    private boolean[] peopleWhoKnows;
 
     public String solve(String input) {
         String[] s = input.split("\n");
-        String[] ss = s[0].split(" ");
-        int numberOfPeople = Integer.parseInt(ss[0]);
-        int numberOfParty = Integer.parseInt(ss[1]);
-        hp = new boolean[numberOfPeople][numberOfParty];
-        know = s[1].length() < 2 ? null : Arrays.stream(s[1].substring(2).split(" "))
-                .mapToInt(Integer::parseInt).toArray();
+        String[] firstLine = s[0].split(" ");
+        int numberOfPeople = Integer.parseInt(firstLine[0]);
+        int numberOfParty = Integer.parseInt(firstLine[1]);
 
-        if (know == null) return String.valueOf(numberOfParty);
+        // If there is no one who knows the truth, then he can lie to every party.
+        if (s[1].equals("0")) return String.valueOf(numberOfParty);
 
-        for (int i = 2; i < s.length; i++) {
-            int person = i - 2;
-            Arrays.stream(s[i].substring(2).split(" "))
-                    .mapToInt(Integer::parseInt).forEach(party -> hp[person][party] = true);
+        graph = IntStream.range(0, numberOfPeople + 1).toArray();
+        peopleWhoKnows = new boolean[numberOfPeople + 1];
+        Arrays.stream(s[1].split(" ")).skip(1).mapToInt(Integer::parseInt).forEach(i -> peopleWhoKnows[i] = true);
+
+        List<List<Integer>> parties = IntStream.range(0, numberOfParty).mapToObj(i -> new ArrayList<Integer>()).collect(Collectors.toList());
+
+        for (int n = 0; n < numberOfParty; n++) {
+            String[] partyMembers = s[n + 2].split(" ");
+            List<Integer> party = parties.get(n);
+
+            party.addAll(Arrays.stream(partyMembers).skip(1).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList()));
+            for (int member = 1; member < Integer.parseInt(partyMembers[0]); member++) union(party.get(member), party.get(member - 1));
         }
 
-        go();
+        boolean[] visited = new boolean[numberOfPeople + 1];
+        for (int i = 1; i <= numberOfPeople; i++) if (peopleWhoKnows[i] && !visited[i])
+            for (int j = 1; j <= numberOfPeople; j++) if (i == j || find(i) == find(j)) {
+                peopleWhoKnows[j] = true;
+                visited[j] = true;
+            }
 
-        int count = 0;
-        for (int j = 0; j < numberOfParty; j++) for (boolean[] booleans : hp)
-            if (booleans[j]) {
-                count++;
+        boolean[] canLie = new boolean[numberOfParty];
+        Arrays.fill(canLie, true);
+        for (int party = 0; party < numberOfParty; party++) for (int people = 1; people < numberOfPeople + 1; people++)
+            if (peopleWhoKnows[people] && parties.get(party).contains(people)) {
+                canLie[party] = false;
                 break;
             }
-        return String.valueOf(numberOfParty - count);
+
+        int sum = 0;
+        for (int i = 0; i < numberOfParty; i++) if (canLie[i]) sum++;
+        return String.valueOf(sum);
     }
 
-    private void go() {
+    private int find(int idx) {
+        if (graph[idx] == idx) return idx;
+        return find(graph[idx]);
+    }
 
-
-
+    private void union(int node1, int node2) {
+        int root1 = find(node1);
+        int root2 = find(node2);
+        if (root1 != root2) graph[root2] = root1;
     }
 }
--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,14 +1,14 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.level.two.BJ18110;
+import org.alan.algorithm.practice.baekjoon.level.four.BJ1043;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BJ18110 instance = new BJ18110();
-        IOUtil.answer(Integer::parseInt, instance::solve);
+        BJ1043 instance = new BJ1043();
+        IOUtil.answer(s -> Integer.parseInt(s.split(" ")[1]) + 1, instance::solve);
     }
 
 
