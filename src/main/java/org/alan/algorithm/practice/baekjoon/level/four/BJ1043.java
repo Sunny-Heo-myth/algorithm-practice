07bcd8ac42b2d3fc6810debec15ad513bb22caf1
sunny <hsymyth@gmail.com>
2023-11-04 18:06:18 +0900

    exercise: BJ1043 - 2

--- src/main/java/org/alan/algorithm/practice/baekjoon/level/four/BJ1043.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/level/four/BJ1043.java
@@ -3,64 +3,62 @@ package org.alan.algorithm.practice.baekjoon.level.four;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class BJ1043 {
+    private boolean[] peopleWhoKnow;
     private int[] graph;
-    private boolean[] peopleWhoKnows;
+    private final List<List<Integer>> parties = new ArrayList<>();
 
     public String solve(String input) {
         String[] s = input.split("\n");
-        String[] firstLine = s[0].split(" ");
-        int numberOfPeople = Integer.parseInt(firstLine[0]);
-        int numberOfParty = Integer.parseInt(firstLine[1]);
-
-        // If there is no one who knows the truth, then he can lie to every party.
+        StringTokenizer st = new StringTokenizer(s[0]);
+        int numberOfPeople = Integer.parseInt(st.nextToken());
+        int numberOfParty = Integer.parseInt(st.nextToken());
         if (s[1].equals("0")) return String.valueOf(numberOfParty);
 
+        peopleWhoKnow = new boolean[numberOfPeople + 1];
+        Arrays.stream(s[1].split(" ")).skip(1).mapToInt(Integer::parseInt).forEach(i -> peopleWhoKnow[i] = true);
         graph = IntStream.range(0, numberOfPeople + 1).toArray();
-        peopleWhoKnows = new boolean[numberOfPeople + 1];
-        Arrays.stream(s[1].split(" ")).skip(1).mapToInt(Integer::parseInt).forEach(i -> peopleWhoKnows[i] = true);
 
-        List<List<Integer>> parties = IntStream.range(0, numberOfParty).mapToObj(i -> new ArrayList<Integer>()).collect(Collectors.toList());
-
-        for (int n = 0; n < numberOfParty; n++) {
-            String[] partyMembers = s[n + 2].split(" ");
-            List<Integer> party = parties.get(n);
-
-            party.addAll(Arrays.stream(partyMembers).skip(1).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList()));
-            for (int member = 1; member < Integer.parseInt(partyMembers[0]); member++) union(party.get(member), party.get(member - 1));
+        for (int i = 0; i < numberOfParty; i++) {
+            parties.add(Arrays.stream(s[i + 2].split(" ")).skip(1).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList()));
+            List<Integer> party = parties.get(i);
+            IntStream.range(1, party.size()).forEach(j -> union(party.get(j), party.get(j - 1)));
         }
 
-        boolean[] visited = new boolean[numberOfPeople + 1];
-        for (int i = 1; i <= numberOfPeople; i++) if (peopleWhoKnows[i] && !visited[i])
-            for (int j = 1; j <= numberOfPeople; j++) if (i == j || find(i) == find(j)) {
-                peopleWhoKnows[j] = true;
+        boolean[] visited = new boolean[numberOfPeople + 1];    // todo:
+        for (int i = 1; i < numberOfPeople + 1; i++) if (peopleWhoKnow[i] && !visited[i]) {
+            for (int j = 1; j < numberOfPeople + 1; j++) if (i == j || find(i) == find(j)) {
                 visited[j] = true;
+                peopleWhoKnow[j] = true;
             }
+        }
 
         boolean[] canLie = new boolean[numberOfParty];
         Arrays.fill(canLie, true);
-        for (int party = 0; party < numberOfParty; party++) for (int people = 1; people < numberOfPeople + 1; people++)
-            if (peopleWhoKnows[people] && parties.get(party).contains(people)) {
+        for (int party = 0; party < numberOfParty; party++) for (int people = 1; people < numberOfPeople + 1; people++) {
+            if (peopleWhoKnow[people] && parties.get(party).contains(people)) {
                 canLie[party] = false;
                 break;
             }
+        }
 
         int sum = 0;
-        for (int i = 0; i < numberOfParty; i++) if (canLie[i]) sum++;
+        for (boolean b : canLie) if (b) sum++;
         return String.valueOf(sum);
     }
 
-    private int find(int idx) {
-        if (graph[idx] == idx) return idx;
-        return find(graph[idx]);
+    private int find(int person) {
+        if (graph[person] == person) return person;
+        return find(graph[person]);
     }
 
-    private void union(int node1, int node2) {
-        int root1 = find(node1);
-        int root2 = find(node2);
+    private void union(int p1, int p2) {
+        int root1 = find(p1);
+        int root2 = find(p2);
         if (root1 != root2) graph[root2] = root1;
     }
 }
