bdf96cc68bc6f91a12e9a81af107c1bed92790eb
sunny <hsymyth@gmail.com>
2023-11-05 20:45:18 +0900

    exericse: BJ1504

--- src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/Main.java
@@ -1,13 +1,13 @@
 package org.alan.algorithm.practice.baekjoon;
 
-import org.alan.algorithm.practice.baekjoon.level.four.BJ1043;
+import org.alan.algorithm.practice.baekjoon.level.four.BJ1504;
 import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BJ1043 instance = new BJ1043();
+        BJ1504 instance = new BJ1504();
         IOUtil.answer(s -> Integer.parseInt(s.split(" ")[1]) + 1, instance::solve);
     }
 
