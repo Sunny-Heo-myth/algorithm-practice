5f4082b7989da1f9fc6d286d8b0bbba5236e4092
sunny <hsymyth@gmail.com>
2023-11-16 15:44:29 +0900

    chore: divideconquer

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/topic/divideconquer/BJ6549.java
@@ -0,0 +1,19 @@
+package org.alan.algorithm.practice.baekjoon.topic.divideconquer;
+
+import java.util.regex.Pattern;
+
+public class BJ6549 {
+    public String solve(String input) {
+        long[] a = Pattern.compile(" ").splitAsStream(input).skip(1).mapToLong(Long::parseLong).toArray();
+        long size = a[0];
+        for (int i = 0; i < a.length; i++) {
+            int j = i;
+            long min = a[j];
+            while (--j >= 0) {
+                min = Long.min(a[j], min);
+                size = Long.max(size, (i - j + 1) * min);
+            }
+        }
+        return String.valueOf(size);
+    }
+}
---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/topic/divideconquer/BJ6549Test.java
@@ -0,0 +1,24 @@
+package org.alan.algorithm.practice.baekjoon.topic.divideconquer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ6549Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("7 2 1 4 5 1 3 3", "8"),
+                Arguments.of("4 1000 1000 1000 1000", "4000")
+        );
+    }
+    
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        BJ6549 instance = new BJ6549();
+        Assertions.assertEquals(expected, instance.solve(input));
+    }
+}
