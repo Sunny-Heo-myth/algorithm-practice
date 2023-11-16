80a418e7a9758b9a595f8993beb48616cff69de9
sunny <hsymyth@gmail.com>
2023-11-16 15:43:36 +0900

    exercise: BJ15829

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/level/two/BJ15829.java
@@ -0,0 +1,16 @@
+package org.alan.algorithm.practice.baekjoon.level.two;
+
+public class BJ15829 {
+    public String solve(String input) {
+        char[] chars = input.split("\n")[1].toCharArray();
+        long sum = 0;
+        long power = 1;
+        long divisor = 1234567891;
+        for (char aChar : chars) {
+            sum += (aChar - 96) * power;
+            power = (power * 31) % divisor;
+        }
+
+        return String.valueOf(sum % divisor);
+    }
+}
