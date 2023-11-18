89bd3e19aa334e60697e742e60c25962f17fa712
sunny <hsymyth@gmail.com>
2023-11-18 18:48:26 +0900

    exercise: BJ2448

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/level/four/BJ2448.java
@@ -0,0 +1,36 @@
+package org.alan.algorithm.practice.baekjoon.level.four;
+
+public class BJ2448 {
+    private final String[] memo = new String[11];
+
+    public String solve(String input) {
+        memo[0] =
+                "  *  \n" +
+                " * * \n" +
+                "*****";
+
+        return recur((int) (Math.log(Integer.parseInt(input)) / Math.log(2) - 1));
+    }
+
+    private String recur(int i) {
+        if (memo[i] != null) return memo[i];
+
+        memo[i] = shift(recur(i - 1)) + "\n" + append(memo[i - 1]);
+        return memo[i];
+    }
+
+    private String shift(String recur) {
+        StringBuilder sb = new StringBuilder();
+        String[] s = recur.split("\n");
+        String repeat = " ".repeat(s[s.length - 1].length() / 2 + 1);
+        for (String ss : s) sb.append(repeat).append(ss).append(repeat).append("\n");
+        return sb.deleteCharAt(sb.length() - 1).toString();
+    }
+
+    private String append(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (String line : s.split("\n")) sb.append(line).append(" ").append(line).append("\n");
+        return sb.deleteCharAt(sb.length() - 1).toString();
+    }
+
+}
---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/level/four/BJ2448Test.java
@@ -0,0 +1,70 @@
+package org.alan.algorithm.practice.baekjoon.level.four;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ2448Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("3",
+                                "  *  \n" +
+                                " * * \n" +
+                                "*****"),
+                Arguments.of("12",
+                                "           *           \n" +
+                                "          * *          \n" +
+                                "         *****         \n" +
+                                "        *     *        \n" +
+                                "       * *   * *       \n" +
+                                "      ***** *****      \n" +
+                                "     *           *     \n" +
+                                "    * *         * *    \n" +
+                                "   *****       *****   \n" +
+                                "  *     *     *     *  \n" +
+                                " * *   * *   * *   * * \n" +
+                                "***** ***** ***** *****"),
+                Arguments.of("24",
+                                "                       *                       \n" +
+                                "                      * *                      \n" +
+                                "                     *****                     \n" +
+                                "                    *     *                    \n" +
+                                "                   * *   * *                   \n" +
+                                "                  ***** *****                  \n" +
+                                "                 *           *                 \n" +
+                                "                * *         * *                \n" +
+                                "               *****       *****               \n" +
+                                "              *     *     *     *              \n" +
+                                "             * *   * *   * *   * *             \n" +
+                                "            ***** ***** ***** *****            \n" +
+                                "           *                       *           \n" +
+                                "          * *                     * *          \n" +
+                                "         *****                   *****         \n" +
+                                "        *     *                 *     *        \n" +
+                                "       * *   * *               * *   * *       \n" +
+                                "      ***** *****             ***** *****      \n" +
+                                "     *           *           *           *     \n" +
+                                "    * *         * *         * *         * *    \n" +
+                                "   *****       *****       *****       *****   \n" +
+                                "  *     *     *     *     *     *     *     *  \n" +
+                                " * *   * *   * *   * *   * *   * *   * *   * * \n" +
+                                "***** ***** ***** ***** ***** ***** ***** *****")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        BJ2448 instance = new BJ2448();
+        Assertions.assertEquals(expected, instance.solve(input));
+    }
+
+    @Test
+    public void logTest() {
+        Assertions.assertEquals((int) (Math.log(3072) / Math.log(2)), 11);
+    }
+}
