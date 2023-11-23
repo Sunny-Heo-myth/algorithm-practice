0294f0c0f57ea53754212d7b8fcf34aaee84dbff
sunny <hsymyth@gmail.com>
2023-11-23 22:04:56 +0900

    exercise: BJ2217

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/topic/greedy/BJ2217.java
@@ -0,0 +1,12 @@
+package org.alan.algorithm.practice.baekjoon.topic.greedy;
+
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+public class BJ2217 {
+    public String solve(String input) {
+        int[] rope = Pattern.compile("\n").splitAsStream(input).skip(1)
+                .mapToInt(Integer::parseInt).sorted().toArray();
+        return String.valueOf(IntStream.range(0, rope.length).map(i -> rope[i] * (rope.length - i)).max().orElse(0));
+    }
+}
---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/topic/greedy/BJ2217Test.java
@@ -0,0 +1,25 @@
+package org.alan.algorithm.practice.baekjoon.topic.greedy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ2217Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("2\n" +
+                        "10\n" +
+                        "15", "20")
+        );
+    }
+    
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        BJ2217 instance = new BJ2217();
+        Assertions.assertEquals(expected, instance.solve(input));
+    }
+}
