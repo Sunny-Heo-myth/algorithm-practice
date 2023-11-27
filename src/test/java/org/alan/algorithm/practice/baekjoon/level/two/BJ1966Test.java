40c7a1ce768d596f0fd5c91b0b60c6faa9916e84
sunny <hsymyth@gmail.com>
2023-11-27 21:40:01 +0900

    exercise: BJ1389, BJ1966

---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/level/two/BJ1966Test.java
@@ -0,0 +1,28 @@
+package org.alan.algorithm.practice.baekjoon.level.two;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ1966Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("1 0\n" +
+                        "5", "1"),
+                Arguments.of("4 2\n" +
+                        "1 2 3 4", "2"),
+                Arguments.of("6 0\n" +
+                        "1 1 9 1 1 1", "5")
+        );
+    }
+    
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        BJ1966 instance = new BJ1966();
+        Assertions.assertEquals(expected, instance.solve(input));
+    }
+}
