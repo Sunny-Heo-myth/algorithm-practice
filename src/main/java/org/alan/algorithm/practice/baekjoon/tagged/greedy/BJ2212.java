cb4f36fb46a882b83fd491f71d26bf3182abf5f4
sunny <hsymyth@gmail.com>
2024-01-11 21:47:36 +0900

    exercise: BJ2212, BJ5582

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/tagged/greedy/BJ2212.java
@@ -0,0 +1,24 @@
+package org.alan.algorithm.practice.baekjoon.tagged.greedy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+public class BJ2212 {
+
+    public String solve(String input) {
+        String[] lines = input.split("\n");
+        int n = Integer.parseInt(lines[0]);
+        int k = Integer.parseInt(lines[1]);
+        if(k >= n) return "0";
+
+        int[] array = Pattern.compile(" ").splitAsStream(lines[2]).mapToInt(Integer::parseInt)
+                .sorted().toArray(); // Nlg(N)
+
+        int[] differ = IntStream.range(0, n - 1).map(i -> array[i + 1] - array[i]).boxed()
+                .sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue).toArray();
+
+        return String.valueOf(Arrays.stream(differ, k - 1, n - 1).sum());
+    }
+}
---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/tagged/greedy/BJ2212Test.java
@@ -0,0 +1,30 @@
+package org.alan.algorithm.practice.baekjoon.tagged.greedy;
+
+import org.alan.algorithm.practice.baekjoon.tagged.greedy.BJ2212;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ2212Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("""
+                        6
+                        2
+                        1 6 9 3 6 7""", "5"),
+                Arguments.of("""
+                        10
+                        5
+                        20 3 14 6 7 8 18 10 12 15""", "7")
+        );
+    }
+    
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ2212().solve(input));
+    }
+}
