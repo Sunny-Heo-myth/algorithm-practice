58d7ad9d0fc4beedbce98775786873adf8b49fb6
alanheo <hsymyth@gmail.com>
2024-03-05 09:36:52 +0900

    exercise: BJ1476

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/silver/BJ1476.java
@@ -0,0 +1,27 @@
+package org.alan.algorithm.practice.baekjoon.silver;
+
+import java.util.StringTokenizer;
+
+public class BJ1476 {
+
+    public String solve(String input) {
+        StringTokenizer st = new StringTokenizer(input);
+        int e = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int E = 1;
+        int S = 1;
+        int M = 1;
+
+        int year = 1;
+        while (!(e == E && s == S && m == M)) {
+            E++; S++; M++;
+            if (E == 16) E = 1;
+            if (S == 29) S = 1;
+            if (M == 20) M = 1;
+            year++;
+        }
+        return String.valueOf(year);
+    }
+}
---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/silver/BJ1476Test.java
@@ -0,0 +1,25 @@
+package org.alan.algorithm.practice.baekjoon.silver;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ1476Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("1 16 16", "16"),
+                Arguments.of("1 1 1", "1"),
+                Arguments.of("1 2 3", "5266"),
+                Arguments.of("15 28 19", "7980")
+        );
+    }
+    
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ1476().solve(input));
+    }
+}
