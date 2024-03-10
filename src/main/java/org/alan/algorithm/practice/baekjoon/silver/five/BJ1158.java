f4517418db6a114fbbcb2eae082527de1b26af6a
alanheo <hsymyth@gmail.com>
2024-03-11 08:16:11 +0900

    exercise: BJ1158

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/silver/five/BJ1158.java
@@ -0,0 +1,25 @@
+package org.alan.algorithm.practice.baekjoon.silver.five;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class BJ1158 {
+    public String solve(String input) {
+        String[] s = input.split(" ");
+        int count = Integer.parseInt(s[1]);
+        Deque<Integer> deque = new ArrayDeque<>();
+        IntStream.rangeClosed(1, Integer.parseInt(s[0])).forEach(deque::add);
+        List<Integer> answer = new ArrayList<>();
+        while (!deque.isEmpty()) {
+            for (int i = 0; i < count - 1; i++) {
+                deque.addLast(deque.poll());
+            }
+            answer.add(deque.poll());
+        }
+        return "<" + answer.stream().map(String::valueOf).collect(Collectors.joining(", ")) + ">";
+    }
+}
---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/silver/five/BJ1158Test.java
@@ -0,0 +1,22 @@
+package org.alan.algorithm.practice.baekjoon.silver.five;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ1158Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("7 3", "<3, 6, 2, 7, 5, 1, 4>")
+        );
+    }
+    
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ1158().solve(input));
+    }
+}
