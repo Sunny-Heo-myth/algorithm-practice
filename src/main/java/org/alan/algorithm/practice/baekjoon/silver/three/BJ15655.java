f1ecf814b4de7e1d4ed89c732f425fb33f3b6b19
sunny <hsymyth@gmail.com>
2024-03-17 21:56:06 +0900

    exercise: BJ15655_2

--- src/main/java/org/alan/algorithm/practice/baekjoon/silver/three/BJ15655.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/silver/three/BJ15655.java
@@ -5,37 +5,30 @@ import java.util.List;
 import java.util.Stack;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BJ15655 {
-    private int length;
-    private int[] ints;
-    private final Stack<Integer> idxList = new Stack<>();
-    private final List<String> answer = new ArrayList<>();
 
     public String solve(String input) {
         String[] lineArray = input.split("\n");
-        length = Integer.parseInt(lineArray[0].split(" ")[1]);
-        ints = Pattern.compile(" ").splitAsStream(lineArray[1]).mapToInt(Integer::parseInt).sorted().toArray();
+        int seqLength = Integer.parseInt(lineArray[0].split(" ")[1]);
+        int[] ints = Pattern.compile(" ").splitAsStream(lineArray[1]).mapToInt(Integer::parseInt).sorted().toArray();
+        Stack<Integer> idxStack = new Stack<>();
+        List<String> answer = new ArrayList<>();
+        if (ints.length > 0 && seqLength > 0) idxStack.push(-1);
 
-        for (int i = 0; i <= ints.length - length; i++) {
-            idxList.push(i);
-            recur(i);
-            idxList.pop();
+        while (!idxStack.isEmpty()) {
+            IntStream.range(idxStack.pop() + 1, ints.length).takeWhile(i -> idxStack.size() < seqLength).forEach(idxStack::push);
+
+            if (idxStack.size() == seqLength) {
+                answer.add(idxStack.stream().map(idx -> String.valueOf(ints[idx])).collect(Collectors.joining(" ")));
+            }
+
+            while (!idxStack.isEmpty() && idxStack.peek() >= ints.length - (seqLength - idxStack.size())) {
+                idxStack.pop();
+            }
         }
 
         return String.join("\n", answer);
-    }
-
-    private void recur(int i) {
-        if (idxList.size() == length) {
-            answer.add(idxList.stream().map(idx -> String.valueOf(ints[idx])).collect(Collectors.joining(" ")));
-            return;
-        }
-
-        for (int j = i + 1; j < ints.length; j++) {
-            idxList.push(j);
-            recur(j);
-            idxList.pop();
-        }
     }
 }
