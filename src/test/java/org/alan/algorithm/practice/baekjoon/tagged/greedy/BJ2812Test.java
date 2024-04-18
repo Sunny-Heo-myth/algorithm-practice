dabf35021a540944a66467a2aee82ab966d99956
alan <hsymyth@gmail.com>
2024-04-18 13:56:19 +0900

    refactor: BJ2812

--- src/test/java/org/alan/algorithm/practice/baekjoon/tagged/greedy/BJ2812Test.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/tagged/greedy/BJ2812Test.java
@@ -1,5 +1,6 @@
 package org.alan.algorithm.practice.baekjoon.tagged.greedy;
 
+import org.alan.algorithm.practice.baekjoon.clazz.five.BJ2812;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
