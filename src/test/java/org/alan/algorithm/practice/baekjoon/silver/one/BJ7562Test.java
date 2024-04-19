7f4f992590ff7f4d5e52f0c5b0bb5e85a0c06936
alan <hsymyth@gmail.com>
2024-04-19 15:45:50 +0900

    exercise: BJ7562

---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/silver/one/BJ7562Test.java
@@ -0,0 +1,33 @@
+package org.alan.algorithm.practice.baekjoon.silver.one;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ7562Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("""
+                        8
+                        0 0
+                        7 0""", "5"),
+                Arguments.of("""
+                        100
+                        0 0
+                        30 50""", "28"),
+                Arguments.of("""
+                        10
+                        1 1
+                        1 1""", "0")
+        );
+    }
+    
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ7562().solve2(input));
+    }
+}
