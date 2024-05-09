de4bd8aa53df48d93f9d3f41a39c6979c73d84fc
alan <hsymyth@gmail.com>
2024-05-09 12:25:13 +0900

    exercise: BJ2531

--- src/main/java/org/alan/algorithm/practice/baekjoon/silver/one/BJ2531.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/silver/one/BJ2531.java
@@ -6,59 +6,39 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class BJ2531 {
+    private Queue<Integer> queue;
+    private int[] kinds;
+    private Queue<Integer> eat;
+
     public String solve(String input) {
         int[] in = Pattern.compile(" ").splitAsStream(input.split("\n")[0]).mapToInt(Integer::parseInt).toArray();
         int N = in[0]; int d = in[1]; int k = in[2]; int c = in[3];
 
-        Queue<Integer> queue = input.lines().skip(1).map(Integer::parseInt).collect(Collectors.toCollection(LinkedList::new));
-        int[] kinds = new int[d + 1];
-        Queue<Integer> eat = new LinkedList<>();
+        queue = input.lines().skip(1).map(Integer::parseInt).collect(Collectors.toCollection(LinkedList::new));
+        kinds = new int[d + 1];
+        eat = new LinkedList<>();
 
         int max = 0;
-        for (int i = 0; i < k; i++) {
-            int dish = queue.poll();
-            eat.offer(dish);
-            if (kinds[dish] == 0) {
-                max++;
-                kinds[dish] = 1;
-            } else {
-                kinds[dish]++;
-            }
-            queue.offer(dish);
-        }
-        if (queue.peek() == c) {
-            max++;
-        }
+        for (int i = 0; i < k; i++) max = update(max);
+        if (kinds[c] == 0 && queue.peek() == c) max++;
 
-        for (int i = 0; i < N; i++) {
-            int currentMax = max;
-
-            int dish = queue.poll();
-            eat.offer(dish);
-            if (kinds[dish] == 0) {
-                currentMax++;
-                kinds[dish] = 1;
-            } else {
-                kinds[dish]++;
-            }
-            queue.offer(dish);
-
-            int polled = eat.poll();
-            if (--kinds[polled] == 0) {
-                currentMax--;
-            }
-
-            if (polled == c) {
-                currentMax++;
-            }
-            if (queue.peek() == c) {
-                currentMax++;
-            }
-
-            max = Math.max(currentMax, max);
+        int cMax = max;
+         for (int i = 0; i < N; i++) {
+            cMax = update(cMax);
+            if (--kinds[eat.poll()] == 0) cMax--;
+            max = Math.max(max, kinds[c] == 0 ? cMax + 1 : cMax);
         }
 
         return String.valueOf(max);
+    }
+
+    private int update(int cMax) {
+        int dish = queue.poll();
+        eat.offer(dish);
+        if (kinds[dish] == 0) cMax++;
+        kinds[dish]++;
+        queue.offer(dish);
+        return cMax;
     }
 
 }
