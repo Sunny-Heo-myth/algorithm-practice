cec7fa9201eccf01287ebba7f40d35dd086b82d6
sunny <hsymyth@gmail.com>
2024-05-10 15:46:59 +0900

    exercise: BJ2302

---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/silver/one/BJ2302Test.java
@@ -0,0 +1,26 @@
+package org.alan.algorithm.practice.baekjoon.silver.one;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ2302Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("""
+                        9
+                        2
+                        4
+                        7""", "12")
+        );
+    }
+    
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ2302().solve(input));
+    }
+}
