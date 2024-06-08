094547169a3eb830aa6bb76b905a52eb1a6c45f1
sunny <hsymyth@gmail.com>
2024-06-08 16:36:10 +0900

    refactor: BJ1339

--- src/main/java/org/alan/algorithm/practice/baekjoon/tagged/string/BJ1339.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/tagged/string/BJ1339.java
@@ -1,20 +1,20 @@
 package org.alan.algorithm.practice.baekjoon.tagged.string;
 
-import java.util.*;
+import java.util.AbstractMap;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class BJ1339 {
     public String solve(String input) {
-        Map<Character, Integer> map = new HashMap<>();
-        input.lines().skip(1).forEach(s -> IntStream.range(0, s.length()).forEach(i -> map.put(
-                s.charAt(i), map.getOrDefault(s.charAt(i), 0) + (int) Math.pow(10, s.length() - i - 1))));
-
-        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort((a, b) -> b.getValue() - a.getValue());
-
-        int[] assignment = new int[26]; int n = 9;
-        for (var entry : list) assignment[entry.getKey() - 'A'] = n--;
+        int[] assignment = new int[26]; AtomicInteger n = new AtomicInteger(9);
+        input.lines().skip(1).flatMap(s -> IntStream.range(0, s.length()).mapToObj(i ->
+                        new AbstractMap.SimpleEntry<>(s.charAt(i), (int) Math.pow(10, s.length() - i - 1))))
+                .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingInt(Map.Entry::getValue)))
+                .entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEach(entry -> assignment[entry.getKey() - 'A'] = n.getAndDecrement());
 
         return String.valueOf(input.lines().skip(1).mapToInt(s -> Integer.parseInt(
                 s.chars().mapToObj(ch -> String.valueOf(assignment[ch - 'A'])).collect(Collectors.joining()))).sum());
