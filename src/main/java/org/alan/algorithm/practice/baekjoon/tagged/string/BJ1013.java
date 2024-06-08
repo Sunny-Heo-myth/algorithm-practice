93afd93cb5d5994b3e5f5a17d5bfe2b3c8ab4c66
sunny <hsymyth@gmail.com>
2024-06-08 19:00:00 +0900

    exercise: BJ1013

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/tagged/string/BJ1013.java
@@ -0,0 +1,24 @@
+package org.alan.algorithm.practice.baekjoon.tagged.string;
+
+public class BJ1013 {
+
+    public String solve(String input) {
+        return recur(input) ? "YES" : "NO";
+    }
+
+    private boolean recur(String parent) {
+        if (parent.startsWith("01")) {
+            if (parent.length() == 2) return true;
+            return recur(parent.substring(2));
+        } else if (parent.matches("100+1+[01]*")) {
+            int s = parent.indexOf('1', 1);
+            int e = s;
+            while (parent.charAt(e) == '1') {
+                if (parent.length() == e + 1) return true;
+                e++;
+            }
+            for (int i = s; i < e; i++) if (recur(parent.substring(i + 1))) return true;
+        }
+        return false;
+    }
+}
