477e72eaabff5fb42497c61bdc0b205c591b1a21
alan <hsymyth@gmail.com>
2024-07-03 17:39:55 +0900

    refactor: BJ3055

--- src/main/java/org/alan/algorithm/practice/baekjoon/gold/four/BJ3055.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/gold/four/BJ3055.java
@@ -7,21 +7,16 @@ import java.util.Stack;
 public class BJ3055 {
     public String solve(String input) {
         int[] dx = {1, -1, 0, 0}; int[] dy = {0, 0, -1, 1};
-        int beaver = 'D'; int hedgehog = 'S'; int rock = 'X'; int water = '*'; int land = '.';
+        int beaver = 'D', hedgehog = 'S', rock = 'X', water = '*', land = '.', visited = '0';
         int[][] map = input.lines().skip(1).map(s -> s.chars().toArray()).toArray(int[][]::new);
 
         int currentTime = 0;
-        boolean[][] visited = new boolean[map.length][map[0].length];
         Queue<int[]> queue = new LinkedList<>();
         Stack<int[]> waterStack = new Stack<>();
 
         for (int i = 0; i < map.length; i++) for (int j = 0; j < map[i].length; j++)
-            if (map[i][j] == hedgehog) {
-                visited[i][j] = true;
-                queue.offer(new int[]{i, j, 0});
-            } else if (map[i][j] == water) {
-                waterStack.push(new int[]{i, j});
-            }
+            if (map[i][j] == hedgehog) queue.offer(new int[]{i, j, 0});
+            else if (map[i][j] == water) waterStack.push(new int[]{i, j});
 
         while (!queue.isEmpty()) {
             int[] poll = queue.poll();
@@ -35,11 +30,10 @@ public class BJ3055 {
                     int wx = pop[0], wy = pop[1];
                     for (int i = 0; i < 4; i++) {
                         int nx = wx + dx[i], ny = wy + dy[i];
-                        if (nx >= 0 && ny >= 0 && nx < map.length && ny < map[0].length
-                                && (map[nx][ny] == land || map[nx][ny] == hedgehog)) {
-                            map[nx][ny] = water;
-                            nextWaterStack.push(new int[]{nx, ny});
-                        }
+                        if (nx < 0 || ny < 0 || nx >= map.length || ny >= map[0].length
+                                || map[nx][ny] == rock || map[nx][ny] == water || map[nx][ny] == beaver) continue;
+                        map[nx][ny] = water;
+                        nextWaterStack.push(new int[]{nx, ny});
                     }
                 }
                 waterStack = nextWaterStack;
@@ -47,12 +41,11 @@ public class BJ3055 {
 
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i], ny = y + dy[i];
-                if (nx >= 0 && ny >= 0 && nx < map.length && ny < map[0].length && !visited[nx][ny]) {
-                    if (map[nx][ny] == beaver) return String.valueOf(time + 1);
-                    if (map[nx][ny] == land || map[nx][ny] == hedgehog) {
-                        visited[nx][ny] = true;
-                        queue.offer(new int[]{nx, ny, time + 1});
-                    }
+                if (nx < 0 || ny < 0 || nx >= map.length || ny >= map[0].length || map[nx][ny] == visited) continue;
+                if (map[nx][ny] == beaver) return String.valueOf(time + 1);
+                else if (map[nx][ny] == land || map[nx][ny] == hedgehog) {
+                    map[nx][ny] = visited;
+                    queue.offer(new int[]{nx, ny, time + 1});
                 }
             }
         }
