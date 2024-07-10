197a4d649d2727532813caf8dfacd782ceae359c
sunny <hsymyth@gmail.com>
2024-07-11 03:57:58 +0900

    exercise: BJ1027

--- src/main/java/org/alan/algorithm/practice/baekjoon/tagged/geometry/BJ1027.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/tagged/geometry/BJ1027.java
@@ -10,33 +10,25 @@ public class BJ1027 {
 
         int max = 0;
         for (int t = 0; t < buildings.length; t++) {
-            int lMax = 0;
-            int cMax = 0;
-            double now = Double.NEGATIVE_INFINITY;
-            for (int i = 0; i < t; i++) {
-                double next = (buildings[t] - buildings[i]) / (t - i);
-                if (next > now) cMax++;
-                else {
-                    lMax = Math.max(lMax, cMax);
-                    cMax = 0;
-                }
+            int lCount = 0;
+            double now = Double.POSITIVE_INFINITY;
+            for (int i = t - 1; i >= 0; i--) {
+                double next = (buildings[i] - buildings[t]) / (i - t);
+                if (next >= now) continue;
+                lCount++;
                 now = next;
             }
 
-            int rMax = 0;
-            cMax = 0;
+            int rCount = 0;
             now = Double.NEGATIVE_INFINITY;
             for (int i = t + 1; i < buildings.length; i++) {
                 double next = (buildings[i] - buildings[t]) / (i - t);
-                if (next > now) cMax++;
-                else {
-                    rMax = Math.max(rMax, cMax);
-                    cMax = 0;
-                }
+                if (next <= now) continue;
+                rCount++;
                 now = next;
             }
 
-            max = Math.max(lMax + rMax, max);
+            max = Math.max(lCount + rCount, max);
         }
         return String.valueOf(max);
     }
