5676f24760c87d07b839d22ef2eedec722c81d54
alan <hsymyth@gmail.com>
2024-07-15 11:01:47 +0900

    study: BJ2812

--- src/main/java/org/alan/algorithm/practice/baekjoon/clazz/five/BJ2812.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/clazz/five/BJ2812.java
@@ -1,26 +1,24 @@
 package org.alan.algorithm.practice.baekjoon.clazz.five;
 
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class BJ2812 {
     public String solve(String input) {
         String[] lines = input.split("\n");
         int k = Integer.parseInt(lines[0].split(" ")[1]);
 
-        int deletableCount = k;
-        String str = lines[1];
+        int count = k;
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < str.length(); i++) {
-            if (!stack.isEmpty()) while (deletableCount > 0 && !stack.empty() && str.charAt(i) > stack.peek()) {
+        for (char ch : lines[1].toCharArray()) {
+            while (!stack.isEmpty() && count > 0 && stack.peek() < ch) {
                 stack.pop();
-                deletableCount--;
+                count--;
             }
-            stack.push(str.charAt(i));
+            stack.push(ch);
         }
 
-        StringBuilder sb = new StringBuilder();
-        while (stack.size() != str.length() - k) stack.pop();
-        while (!stack.isEmpty()) sb.append(stack.pop());
-        return sb.reverse().toString();
+        while (stack.size() > lines[1].length() - k) stack.pop();
+        return stack.stream().map(String::valueOf).collect(Collectors.joining());
     }
 }
