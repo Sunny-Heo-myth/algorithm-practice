f0cb98974f98fa74a916af1ea25c7764184d30a8
alan <hsymyth@gmail.com>
2024-07-22 20:22:46 +0900

    exercise: BJ1701

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/tagged/string/BJ1701.java
@@ -0,0 +1,26 @@
+package org.alan.algorithm.practice.baekjoon.tagged.string;
+
+import java.util.stream.IntStream;
+
+public class BJ1701 {
+    public String solve(String input) {
+        return String.valueOf(IntStream.range(0, input.length())
+                .map(i -> getPreSufMax(input.substring(i))).max().orElse(0));
+    }
+
+    private int getPreSufMax(String pattern) {
+        int l = 0, n = pattern.length(), max = 0;
+        int[] pi = new int[n];
+
+        for (int r = 1; r < n; r++) {
+            while (l > 0 && pattern.charAt(l) != pattern.charAt(r)) l = pi[l - 1];
+
+            if (pattern.charAt(l) == pattern.charAt(r)) {
+                pi[r] = ++l;
+                max = Math.max(max, pi[r]);
+            }
+        }
+        return max;
+    }
+
+}
