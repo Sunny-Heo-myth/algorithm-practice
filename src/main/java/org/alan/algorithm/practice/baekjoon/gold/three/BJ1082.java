fa49decffec5ec0a7b24e7bfbf142423017bcdf3
alan <hsymyth@gmail.com>
2024-08-05 20:10:02 +0900

    exercise: BJ1082

--- src/main/java/org/alan/algorithm/practice/baekjoon/gold/three/BJ1082.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/gold/three/BJ1082.java
@@ -12,28 +12,27 @@ public class BJ1082 {
                 .mapToInt(Integer::parseInt).toArray()).toArray(int[][]::new);
         int[] costs = lines[1];
         if (costs.length <= 1) return "0";
-        int moneyLeft = lines[2][0];
 
-        int first = IntStream.range(1, costs.length).boxed().min(Comparator.comparingInt(i -> costs[i])).orElse(-1);
+        int money = lines[2][0];
+        int firstN = IntStream.range(1, costs.length).boxed().min(Comparator.comparingInt(i -> costs[i])).orElse(-1);
+        if (costs[firstN] > money) return "0";
+
         int min = IntStream.range(0, costs.length).boxed().min(Comparator.comparingInt(i -> costs[i])).orElse(-1);
-        int numberOfCard = 1 + (moneyLeft - costs[first]) / costs[min];
-        if (numberOfCard <= 0) return "0";
+        int[] answer = new int[(money - costs[firstN]) / costs[min] + 1];
 
-        int[] answer = new int[numberOfCard];
-        answer[0] = first;
+        answer[0] = firstN;
+        money -= costs[firstN];
         for (int i = 1; i < answer.length; i++) answer[i] = min;
-        moneyLeft -= (costs[first] + (numberOfCard - 1) * costs[min]);
+        money -= (money / costs[min]) * costs[min];
 
         for (int i = 0; i < answer.length; i++) {
-            int old = answer[i];
-            for (int j = costs.length - 1; j >= 0; j--) if ((costs[j] - costs[old]) <= moneyLeft) {
-                answer[i] = j;
+            int oldN = answer[i];
+            for (int newN = costs.length - 1; newN > oldN; newN--) if ((costs[newN] - costs[oldN]) <= money) {
+                answer[i] = newN;
+                money -= (costs[newN] - costs[oldN]);
                 break;
             }
-            if (old == answer[i]) return Arrays.stream(answer).mapToObj(String::valueOf).collect(Collectors.joining());
-            moneyLeft -= (costs[answer[i]] - costs[old]);
         }
-
         return Arrays.stream(answer).mapToObj(String::valueOf).collect(Collectors.joining());
     }
 }
