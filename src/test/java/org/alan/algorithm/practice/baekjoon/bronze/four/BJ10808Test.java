cad2b91deeadde8e8b83d43171b99fc5d738fe16
sunny <hsymyth@gmail.com>
2024-08-21 23:06:09 +0900

    exercise: BJ18080

---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/bronze/four/BJ10808Test.java
@@ -0,0 +1,22 @@
+package org.alan.algorithm.practice.baekjoon.bronze.four;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ10808Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("baekjoon", "1 1 0 0 1 0 0 0 0 1 1 0 0 1 2 0 0 0 0 0 0 0 0 0 0 0")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ10808().solve(input));
+    }
+}
