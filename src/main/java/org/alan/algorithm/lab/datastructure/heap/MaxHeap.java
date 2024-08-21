d4c6628fcaeca2d6fd48c0b2936e8b4470496b0c
alan <hsymyth@gmail.com>
2024-08-21 09:44:59 +0900

    study: heap

---
+++ src/main/java/org/alan/algorithm/lab/datastructure/heap/MaxHeap.java
@@ -0,0 +1,74 @@
+package org.alan.algorithm.lab.datastructure.heap;
+
+public class MaxHeap {
+    private int[] arr;
+    private int size;
+    private int maxSize;
+
+    public MaxHeap(int maxSize) {
+        this.maxSize = maxSize;
+        this.size = 0;
+        this.arr = new int[maxSize + 1];
+        this.arr[0] = Integer.MAX_VALUE;
+    }
+
+    private int parent(int i) {
+        return i / 2;
+    }
+
+    private int leftChild(int i) {
+        return 2 * i;
+    }
+
+    private int rightChild(int i) {
+        return 2 * i + 1;
+    }
+
+    private boolean isLeaf(int i) {
+        return i > (size / 2) && i <= size;
+    }
+
+    private void swap(int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    private void maxHeapify(int i) {
+        if (isLeaf(i)) return;
+
+        int l = leftChild(i);
+        int r = rightChild(i);
+
+        int bigger = i;
+        if (l <= size && arr[l] > arr[bigger]) bigger = l;
+        if (r <= size && arr[r] > arr[bigger]) bigger = r;
+        if (bigger == i) return;
+
+        swap(i, bigger);
+        maxHeapify(bigger);
+    }
+
+    public int poll() {
+        int polled = arr[1];
+        arr[1] = arr[size--];
+        maxHeapify(1);
+        return polled;
+    }
+
+    public int peek() {
+        return arr[1];
+    }
+
+    public void insert(int v) {
+        if (size >= maxSize) throw new RuntimeException("MaxHeap is full.");
+
+        arr[++size] = v;
+
+        int i = size;
+        while (arr[i] > arr[parent(v)]) {
+            swap(i, parent(i));
+            i = parent(i);
+        }
+    }
+}
---
+++ src/main/java/org/alan/algorithm/lab/datastructure/heap/MinHeap.java
@@ -0,0 +1,76 @@
+package org.alan.algorithm.lab.datastructure.heap;
+
+public class MinHeap {
+    private int[] arr;
+    private int size;
+    private int capacity;
+
+    public MinHeap(int capacity) {
+        this.arr = new int[capacity + 1];
+        arr[0] = Integer.MIN_VALUE;
+        this.size = 0;
+        this.capacity = capacity;
+    }
+
+    private int parent(int i) {
+        return i / 2;
+    }
+
+    private int left(int i) {
+        return 2 * i;
+    }
+
+    private int right(int i) {
+        return 2 * i + 1;
+    }
+
+    private boolean isLeaf(int i) {
+        return i > size / 2  && i <= size;
+    }
+
+    private void swap(int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public int peek() {
+        return arr[1];
+    }
+
+    private void minHeapify(int i) {
+        if (isLeaf(i)) return;
+
+        int l = left(i);
+        int r = right(i);
+
+        int smaller = i;
+        if (l <= size && arr[l] < arr[smaller]) smaller = l;
+        if (r <= size && arr[r] < arr[smaller]) smaller = r;
+        if (smaller == i) return;
+
+        swap(i, smaller);
+        minHeapify(smaller);
+    }
+
+    public void insert(int v) {
+        if (size >= capacity) throw new RuntimeException("MinHeap is full");
+
+        arr[++size] = v;
+
+        int i = size;
+        while (arr[i] < arr[parent(i)]) {
+            swap(i, parent(i));
+            i = parent(i);
+        }
+    }
+
+    public int poll() {
+        int polled = arr[1];
+        arr[1] = arr[size--];
+        minHeapify(1);
+        return polled;
+    }
+
+
+}
