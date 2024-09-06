b3f63d96bbac000c47353a4885bfa25d0249b620
sunny <hsymyth@gmail.com>
2024-09-06 19:23:55 +0900

    exercise: BJ1300

---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/tagged/bsearch/BJ1300Test.java
@@ -0,0 +1,24 @@
+package org.alan.algorithm.practice.baekjoon.tagged.bsearch;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ1300Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("""
+                        3
+                        7""", "6")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ1300().solve(input));
+    }
+}
