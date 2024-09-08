feac65803f8c63b52e16b63096753954a758b3a2
sunny <hsymyth@gmail.com>
2024-09-08 10:52:10 +0900

    refactor: BJ11066

--- src/main/java/org/alan/algorithm/practice/baekjoon/tagged/dynamic/BJ11066.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/tagged/dynamic/BJ11066.java
@@ -13,10 +13,9 @@ public class BJ11066 {
 
         int[][] memo = new int[k + 1][k + 1];
         for (int n = 1; n <= k; n++) for (int i = 1; i + n <= k; i++) {
-            int to = i + n;
-            int from = i;
-            memo[i][to] = IntStream.range(i, to)
-                    .map(div -> memo[from][div] + memo[div + 1][to] + sum[to] - sum[from - 1])
+            int from = i, to = i + n;
+            memo[from][to] = IntStream.range(from, to)
+                    .map(mid -> memo[from][mid] + memo[mid + 1][to] + sum[to] - sum[from - 1])
                     .min().orElse(-1);
         }
         return String.valueOf(memo[1][k]);
