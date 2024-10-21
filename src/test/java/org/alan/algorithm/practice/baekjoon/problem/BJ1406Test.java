b1acaabbeedecfd80494df3f085931249c74426d
sunny <hsymyth@gmail.com>
2024-10-21 09:35:17 +0900

    exercise: BJ1406

---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/problem/BJ1406Test.java
@@ -0,0 +1,53 @@
+package org.alan.algorithm.practice.baekjoon.problem;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ1406Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("""
+                        abcd
+                        3
+                        P x
+                        L
+                        P y""", "abcdyx"),
+                Arguments.of("""
+                        abc
+                        9
+                        L
+                        L
+                        L
+                        L
+                        L
+                        P x
+                        L
+                        B
+                        P y""", "yxabc"),
+                Arguments.of("""
+                        dmih
+                        11
+                        B
+                        B
+                        P x
+                        L
+                        B
+                        B
+                        B
+                        P y
+                        D
+                        D
+                        P z""", "yxz")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ1406().solve(input));
+    }
+}
