b97accda5580402c6601b7e40f1d5af3cdeb6d43
alan <hsymyth@gmail.com>
2024-10-21 18:52:01 +0900

    exercise: BJ5397

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/problem/BJ5397.java
@@ -0,0 +1,34 @@
+package org.alan.algorithm.practice.baekjoon.problem;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.Collectors;
+
+public class BJ5397 {
+    public String solve(String input) {
+        List<Character> result = new LinkedList<>();
+        ListIterator<Character> iterator = result.listIterator();
+
+        for (char c : input.toCharArray()) {
+            switch (c) {
+                case '<':
+                    if (iterator.hasPrevious()) iterator.previous();
+                    break;
+                case '>':
+                    if (iterator.hasNext()) iterator.next();
+                    break;
+                case '-':
+                    if (iterator.hasPrevious()) {
+                        iterator.previous();
+                        iterator.remove();
+                    }
+                    break;
+                default:
+                    iterator.add(c);
+            }
+        }
+
+        return result.stream().map(String::valueOf).collect(Collectors.joining());
+    }
+}
---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/problem/BJ5397Test.java
@@ -0,0 +1,23 @@
+package org.alan.algorithm.practice.baekjoon.problem;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ5397Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("<<BP<A>>Cd-", "BAPC"),
+                Arguments.of("ThIsIsS3Cr3t", "ThIsIsS3Cr3t")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ5397().solve(input));
+    }
+}
