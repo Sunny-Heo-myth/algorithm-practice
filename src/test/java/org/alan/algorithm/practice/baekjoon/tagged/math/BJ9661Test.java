396d9c94fc6f3374317c0ea28c0cf1eaa8d95c93
sunny <hsymyth@gmail.com>
2025-01-04 00:17:10 +0900

    exercise: BJ9661

---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/tagged/math/BJ9661Test.java
@@ -0,0 +1,22 @@
+package org.alan.algorithm.practice.baekjoon.tagged.math;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ9661Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("3", "SK")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ9661().solve(input));
+    }
+}
