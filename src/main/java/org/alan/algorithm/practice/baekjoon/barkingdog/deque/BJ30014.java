0e8b038b1d9b2f2dad90e97dd004237005287efa
alan <hsymyth@gmail.com>
2025-01-15 11:13:05 +0900

    exercise: BJ30014

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/barkingdog/deque/BJ30014.java
@@ -0,0 +1,30 @@
+package org.alan.algorithm.practice.baekjoon.barkingdog.deque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class BJ30014 {
+    public String solve(String input) {
+        int[] pearls = Pattern.compile(" ").splitAsStream(input.split("\n")[1])
+                .mapToInt(Integer::parseInt)
+                .sorted().toArray();
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        boolean fromFirst = true;
+        for (int value : pearls) {
+            if (fromFirst) deque.offerFirst(value);
+            else deque.offerLast(value);
+            fromFirst = !fromFirst;
+        }
+
+        int sum = 0, i = 0, size = deque.size();
+        while (i++ < size) {
+            int first = deque.pollFirst(), second = deque.peekFirst();
+            deque.offerLast(first);
+            sum += (first * second);
+        }
+        return sum + "\n" + deque.stream().map(String::valueOf).collect(Collectors.joining(" "));
+    }
+}
---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/barkingdog/deque/BJ30014Test.java
@@ -0,0 +1,41 @@
+package org.alan.algorithm.practice.baekjoon.barkingdog.deque;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ30014Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("""
+                        3
+                        2 3 5""", """
+                        31
+                        5 2 3"""),
+                Arguments.of("""
+                        3
+                        1000 1000 1000""", """
+                        3000000
+                        1000 1000 1000"""),
+                Arguments.of("""
+                        214
+                        1 2 4 8 16""", """
+                        5
+                        16 4 1 2 8"""),
+                Arguments.of("""
+                        5
+                        1 2 3 4 5""", """
+                        48
+                        5 3 1 2 4""")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ30014().solve(input));
+    }
+}
