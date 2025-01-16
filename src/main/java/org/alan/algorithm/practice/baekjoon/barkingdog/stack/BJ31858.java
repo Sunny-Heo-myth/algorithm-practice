f4908b06d47811ffb18355061764d0faf17a2631
alan <hsymyth@gmail.com>
2025-01-16 11:35:59 +0900

    exercise: BJ31858

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/barkingdog/stack/BJ31858.java
@@ -0,0 +1,38 @@
+package org.alan.algorithm.practice.baekjoon.barkingdog.stack;
+
+import java.util.Stack;
+import java.util.regex.Pattern;
+
+public class BJ31858 {
+    public String solve(String input) {
+        int[] arr = Pattern.compile(" ").splitAsStream(input.split("\n")[1])
+                .mapToInt(Integer::parseInt).toArray();
+        Stack<Integer> stack = new Stack<>();
+
+        // initiation :
+        //  - Stack is empty.
+        // after loop :
+        //  - Stack peek is n.
+        //  - All count including case with second element as n has been considered.
+        // termination :
+        //  - All count considered starting from the first to the last element as the second element of a case.
+        int count = 0;
+        for (int n : arr) {
+            // initiation :
+            //  - n is the second element of a possible case.
+            //  - Continuous stack pop will give numbers with ascending order.
+            // after loop :
+            //  - Counting with n and stack pop has been considered.
+            // termination :
+            //  - If stack is not empty, stack peek is bigger than n.
+            //  - If stack is empty, all possible counting of the cases with second element as n has been considered.
+            while (!stack.isEmpty() && stack.peek() < n) {
+                stack.pop();
+                count++;
+            }
+            if (!stack.isEmpty()) count++;
+            stack.push(n);
+        }
+        return String.valueOf(count);
+    }
+}
---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/barkingdog/stack/BJ31858Test.java
@@ -0,0 +1,63 @@
+package org.alan.algorithm.practice.baekjoon.barkingdog.stack;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ31858Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("""
+                        1
+                        1""", "0"),
+                Arguments.of("""
+                        2
+                        1 2""", "1"),
+                Arguments.of("""
+                        2
+                        2 1""", "1"),
+                Arguments.of("""
+                        3
+                        1 2 3""", "2"),
+                Arguments.of("""
+                        3
+                        2 1 3""", "3"),
+                Arguments.of("""
+                        4
+                        3 2 1 4""", "5"),
+                Arguments.of("""
+                        5
+                        1 2 3 4 5""", "4"),
+                Arguments.of("""
+                        5
+                        5 4 3 2 1""", "4"),
+                Arguments.of("""
+                        5
+                        1 3 2 4 5""", "5"),
+                Arguments.of("""
+                        5
+                        1 3 5 4 2""", "4"),
+                Arguments.of("""
+                        5
+                        1 4 5 2 3""", "4"),
+                Arguments.of("""
+                        5
+                        5 3 1 2 4""", "7"),
+                Arguments.of("""
+                        5
+                        4 2 3 1 5""", "7"),
+                Arguments.of("""
+                        5
+                        3 2 1 4 5""", "6")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ31858().solve(input));
+    }
+}
