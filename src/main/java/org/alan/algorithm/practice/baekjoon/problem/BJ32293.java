adb216d1d1087a828d40e3c76a58aee56d5b660a
alan <hsymyth@gmail.com>
2025-01-17 14:06:20 +0900

    exercise: BJ32293

---
+++ src/main/java/org/alan/algorithm/practice/baekjoon/problem/BJ32293.java
@@ -0,0 +1,47 @@
+package org.alan.algorithm.practice.baekjoon.problem;
+
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+public class BJ32293 {
+    private static final int A = 'A', B = 'B';
+
+    public String solve(String input) {
+        int[] chars = input.split("\n")[1].chars().toArray();
+        Stack<Integer> stack = new Stack<>();
+
+        // initiation: No characters are included for answer string, thus stack is empty.
+        // after loop: 'ch' has been included to answer string stack represent calculated string in order.
+        // termination: All characters are included for answer string, thus stack represents the result.
+        for (int ch : chars) {
+            stack.push(ch);
+
+            // initiation:
+            //  - Stack is calculated before including ch.
+            //  - No transition found yet, count is 0.
+            // after loop:
+            //  - A transition has been found. Count incremented.
+            //  - A single 'B' pushed to the stack since it can be used for next transition.
+            // termination:
+            //  - All possible transition has been calculated.
+            //  - Count represent the number of transition found by including ch.
+            int count = 0;
+            while (stack.size() >= 3) {
+                int first = stack.pop(), second = stack.pop(), third = stack.peek();
+                if (first == B && second == B && third == A) {
+                    stack.pop();
+                    stack.push(B);
+                    count++;
+                } else {
+                    stack.push(second);
+                    stack.push(first);
+                    break;
+                }
+            }
+
+            for (int i = 0; i < count; i++) stack.push(A);
+        }
+
+        return stack.stream().map(i -> String.valueOf((char) i.intValue())).collect(Collectors.joining());
+    }
+}
---
+++ src/test/java/org/alan/algorithm/practice/baekjoon/problem/BJ32293Test.java
@@ -0,0 +1,45 @@
+package org.alan.algorithm.practice.baekjoon.problem;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BJ32293Test {
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("""
+                        1
+                        A""", "A"),
+                Arguments.of("""
+                        1
+                        B""", "B"),
+                Arguments.of("""
+                        2
+                        AB""", "AB"),
+                Arguments.of("""
+                        3
+                        ABA""", "ABA"),
+                Arguments.of("""
+                        3
+                        BBA""", "BBA"),
+                Arguments.of("""
+                        3
+                        ABB""", "BA"),
+                Arguments.of("""
+                        9
+                        ABABABBBB""", "BAABA"),
+                Arguments.of("""
+                        12
+                        AAAAAABBBBBB""", "AAAABABA")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ32293().solve(input));
+    }
+}
