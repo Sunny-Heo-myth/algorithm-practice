e841adc669e9cbd987cf2754d90963e77bd80a4b
alan <hsymyth@gmail.com>
2025-03-24 13:53:09 +0900

    review: BJ6549

--- src/main/java/org/alan/algorithm/practice/baekjoon/tagged/sqdl/BJ6549.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/tagged/sqdl/BJ6549.java
@@ -1,19 +1,34 @@
 package org.alan.algorithm.practice.baekjoon.tagged.sqdl;
 
-import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.Stack;
 
 public class BJ6549 {
-    public String solve(String input) {
-        long[] a = Pattern.compile(" ").splitAsStream(input).skip(1).mapToLong(Long::parseLong).toArray();
-        long size = a[0];
-        for (int i = 0; i < a.length; i++) {
-            int j = i;
-            long min = a[j];
-            while (--j >= 0) {
-                min = Long.min(a[j], min);
-                size = Long.max(size, (i - j + 1) * min);
+    private final long[] arr;
+
+    public BJ6549(String input) {
+        arr = Arrays.stream(input.split(" ")).skip(1).mapToLong(Long::parseLong).toArray();
+    }
+
+    public String solve2() {
+        long max = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        int i = 0;
+        while (i < arr.length) {
+            if (stack.isEmpty() || arr[stack.peek()] <= arr[i]) {
+                stack.push(i++);
+            } else {
+                long area = arr[stack.pop()] * (stack.isEmpty() ? i : i - stack.peek() - 1);
+                max = Math.max(max, area);
             }
         }
-        return String.valueOf(size);
+
+        while (!stack.isEmpty()) {
+            long area = arr[stack.pop()] * (stack.isEmpty() ? arr.length : arr.length - stack.peek() - 1);
+            max = Math.max(max, area);
+        }
+        return String.valueOf(max);
     }
+
 }
--- src/test/java/org/alan/algorithm/practice/baekjoon/tagged/sqdl/BJ6549Test.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/tagged/sqdl/BJ6549Test.java
@@ -10,15 +10,32 @@ import java.util.stream.Stream;
 public class BJ6549Test {
     static Stream<Arguments> parameters() {
         return Stream.of(
+                Arguments.of("3 100 50 100", "150"),
+                Arguments.of("3 5 2 5", "6"),
                 Arguments.of("7 2 1 4 5 1 3 3", "8"),
-                Arguments.of("4 1000 1000 1000 1000", "4000")
+                Arguments.of("4 1000 1000 1000 1000", "4000"),
+                Arguments.of("2 1 3", "3"),
+                Arguments.of("4 1 5 1 4", "5"),
+                Arguments.of("3 1 5 1", "5"),
+                Arguments.of("3 0 1 0", "1"),
+                Arguments.of("9 4 6 8 10 10 9 7 5 3", "36"),
+                Arguments.of("9 4 6 8 10 0 9 7 5 3", "18"),
+                Arguments.of("9 4 6 8 0 10 9 7 5 3", "21"),
+                Arguments.of("9 4 6 8 0 10 9 0 5 3", "18"),
+                Arguments.of("9 10 10 0 10 10 9 0 5 3", "27")
         );
     }
     
+//    @ParameterizedTest
+//    @MethodSource("parameters")
+//    public void test1(String input, String expected) {
+//        Assertions.assertEquals(expected, new BJ6549(input).solve());
+//    }
+
     @ParameterizedTest
     @MethodSource("parameters")
-    public void test1(String input, String expected) {
-        BJ6549 instance = new BJ6549();
-        Assertions.assertEquals(expected, instance.solve(input));
+    public void test2(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ6549(input).solve2());
     }
+
 }
