c23c256825da36171b892ed9c10f466a66d619de
alan <hsymyth@gmail.com>
2025-04-01 11:40:24 +0900

    refactor: BJ25304

--- src/main/java/org/alan/algorithm/practice/baekjoon/stepbystep/forfor/BJ25304.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/stepbystep/forfor/BJ25304.java
@@ -1,27 +1,13 @@
 package org.alan.algorithm.practice.baekjoon.stepbystep.forfor;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class BJ25304 {
+    private final String[] lines;
 
-    public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        sb.append(bf.readLine()).append("\n");
-        String numberOfLines = bf.readLine();
-        sb.append(numberOfLines).append("\n");
-
-        for (int i = 0; i < Integer.parseInt(numberOfLines); i++) {
-            sb.append(bf.readLine()).append("\n");
-        }
-        System.out.print(new BJ25304().solve(sb.toString()));
+    public BJ25304(String input) {
+        lines = input.split("\n");
     }
 
-    public String solve(String input) {
-        String[] lines = input.split("\n");
+    public String solve() {
         int totalPrice = Integer.parseInt(lines[0]);
         int kinds = Integer.parseInt(lines[1]);
         int sum = 0;
--- src/test/java/org/alan/algorithm/practice/baekjoon/stepbystep/forfor/BJ25304Test.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/stepbystep/forfor/BJ25304Test.java
@@ -2,31 +2,37 @@ package org.alan.algorithm.practice.baekjoon.stepbystep.forfor;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class BJ25304Test {
-    BJ25304 bj25304 = new BJ25304();
 
-    @Test
-    public void solveTest1() {
-        Assertions.assertEquals("Yes", bj25304.solve(
-                "260000\n" +
-                "4\n" +
-                "20000 5\n" +
-                "30000 2\n" +
-                "10000 6\n" +
-                "5000 8"
-        ));
+    static Stream<Arguments> test_cases() {
+        return Stream.of(
+                Arguments.of("""
+                        260000
+                        4
+                        20000 5
+                        30000 2
+                        10000 6
+                        5000 8""", "Yes"),
+                Arguments.of("""
+                        250000
+                        4
+                        20000 5
+                        30000 2
+                        10000 6
+                        5000 8""", "No")
+        );
     }
 
-    @Test
-    public void solveTest2() {
-        Assertions.assertEquals("No", bj25304.solve(
-                "250000\n" +
-                        "4\n" +
-                        "20000 5\n" +
-                        "30000 2\n" +
-                        "10000 6\n" +
-                        "5000 8"
-        ));
+    @ParameterizedTest
+    @MethodSource("test_cases")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ25304(input).solve());
     }
+
 }
