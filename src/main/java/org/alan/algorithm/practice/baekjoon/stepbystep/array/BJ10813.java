055838df69b5111acb50043becca3a88877bf7a5
alan <hsymyth@gmail.com>
2025-08-24 21:08:37 +0900

    chore: BJ10813

--- src/main/java/org/alan/algorithm/practice/baekjoon/stepbystep/array/BJ10813.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/stepbystep/array/BJ10813.java
@@ -1,9 +1,5 @@
 package org.alan.algorithm.practice.baekjoon.stepbystep.array;
 
-import org.alan.algorithm.practice.baekjoon.utils.IOUtil;
-
-import java.io.IOException;
-
 public class BJ10813 {
     private final int[] array;
 
@@ -11,9 +7,7 @@ public class BJ10813 {
         String[] lines = input.split("\n");
         String[] firstLine = lines[0].split(" ");
         array = new int[Integer.parseInt(firstLine[0])];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i + 1;
-        }
+        for (int i = 0; i < array.length; i++) array[i] = i + 1;
 
         int i = 0;
         while (i < Integer.parseInt(firstLine[1])) {
@@ -27,18 +21,12 @@ public class BJ10813 {
         }
     }
 
-    @Override
-    public String toString() {
+    public String solve() {
+
         StringBuilder sb = new StringBuilder();
         for (int j : array) {
             sb.append(j).append(" ");
         }
         return sb.deleteCharAt(sb.length() - 1).toString();
-    }
-
-    public static void main(String[] args) throws IOException {
-        String input = IOUtil.readFiniteLine(firstLine -> Integer.parseInt(firstLine.split(" ")[1]));
-        BJ10813 instance = new BJ10813(input);
-        System.out.print(instance);
     }
 }
