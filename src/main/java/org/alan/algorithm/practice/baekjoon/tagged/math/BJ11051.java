5cd526e8f35e288b8ec55db4b5a841002001a8f4
alan <hsymyth@gmail.com>
2025-09-06 00:06:48 +0900

    exercise: BJ11051

--- src/main/java/org/alan/algorithm/practice/baekjoon/tagged/math/BJ11051.java
+++ src/main/java/org/alan/algorithm/practice/baekjoon/tagged/math/BJ11051.java
@@ -1,4 +1,19 @@
 package org.alan.algorithm.practice.baekjoon.tagged.math;
 
 public class BJ11051 {
+    private final int n, k;
+    private final int[] memo;
+
+    public BJ11051(String input) {
+        String[] ss = input.split(" ");
+        n = Integer.parseInt(ss[0]); k = Integer.parseInt(ss[1]);
+        memo = new int[n + 1];
+    }
+
+    public String solve() {
+        for (int i = 1; i <= n; i++) for (int j = i; j >= 0; j--)
+            memo[j] = (j == i || j == 0) ? 1 : (memo[j] + memo[j - 1]) % 10_007;
+        return String.valueOf(memo[k]);
+    }
+
 }
--- src/test/java/org/alan/algorithm/practice/baekjoon/tagged/math/BJ11051Test.java
+++ src/test/java/org/alan/algorithm/practice/baekjoon/tagged/math/BJ11051Test.java
@@ -1,4 +1,31 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.alan.algorithm.practice.baekjoon.tagged.math;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 class BJ11051Test {
-  
+    
+    static Stream<Arguments> test_cases() {
+        return Stream.of(
+                Arguments.of("1 0", "1"),
+                Arguments.of("2 0", "1"),
+                Arguments.of("1 1", "1"),
+                Arguments.of("2 2", "1"),
+                Arguments.of("2 1", "2"),
+                Arguments.of("3 1", "3"),
+                Arguments.of("3 2", "3"),
+                Arguments.of("5 2", "10")
+        );
+    }
+    
+    @ParameterizedTest
+    @MethodSource("test_cases")
+    public void test1(String input, String expected) {
+        Assertions.assertEquals(expected, new BJ11051(input).solve());
+    }
+
 }
